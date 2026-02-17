86c42e7ca22bf5e1b0d35ffabd806fc9310ee3c8
Piyushjha45 <pj5695@srmist.edu.in>
2026-02-17 11:21:58 +0530

    Added second banner program

--- PrintBanner.java
+++ PrintBanner.java
@@ -2,13 +2,13 @@ public class PrintBanner {
 
     public static void main(String[] args) {
 
-        System.out.println(" **   **   **   ** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * **   **   ***  ");
-        System.out.println("*     * *       *       *      ");
-        System.out.println("*     * *       *       *      ");
-        System.out.println(" ***  *       *       *      ");
+        System.out.println(" *******  *******   *******   *******");
+        System.out.println(" *     *  *     *   *     *   *      ");
+        System.out.println(" *     *  *     *   *     *   *      ");
+        System.out.println(" *     *  *     *   *******   *******");
+        System.out.println(" *     *  *     *   *               *");
+        System.out.println(" *     *  *     *   *               *");
+        System.out.println(" *******  *******   *         *******");
 
     }
 }
